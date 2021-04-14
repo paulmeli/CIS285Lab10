8e1f175807c936258a3ee5b11206614d6e1a2093
Melissa Paul <paulmeli@umich.edu>
2021-04-14 19:58:54 -0400

    Coded testPositive and testNegative methods

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -22,26 +22,52 @@ public class testSelectionSort {
     }
 
     public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
+      int[] arr = new int[5];
+      arr[0] = 8;
+      arr[1] = 9;
+      arr[2] = 7;
+      arr[3] = 10;
+      arr[4] = 2;
 
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+      int[] Sortedarr = new int[5];
+      Sortedarr[0] = 2;
+      Sortedarr[1] = 7;
+      Sortedarr[2] = 8;
+      Sortedarr[3] = 9;
+      Sortedarr[4] = 10;
 
-        /** add tests to check for this unit test **/
+      SelectionSort temp1 = new SelectionSort();
+      int[] testSortedArr = temp1.basicSelectionSort(arr);
+
+      /** add tests to check for this unit test **/
+
+      for (int i = 0; i < Sortedarr.length; i++)
+        assertEquals("testPositive failed at " + i, Sortedarr[i], testSortedArr[i]);
     }
 
     public void testNegative(){
+      /** Test data contains negative values only **/
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
 
-        /** Test data contains negative values only **/
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        SelectionSort temp1 = new SelectionSort();
+        int[] testSortedArr = temp1.basicSelectionSort(arr);
+
+        /** add tests to check for this unit test **/
+
+        for (int i = 0; i < Sortedarr.length; i++)
+        	assertEquals("testNegative failed at " + i, Sortedarr[i], testSortedArr[i]);
     }
 
     public void testMixed(){
